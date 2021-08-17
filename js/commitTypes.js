af9507bda06634b8a9b616e808524ee75ac4665c
Lucas Nicolau <fedhrm@gmail.com>
2021-08-17 19:37:21 -0300

    ✨ Add commit type list

---
+++ js/commitTypes.js
@@ -0,0 +1,272 @@
+var commitTypes = [
+    {
+        emoji: "🎉",
+        github: "tada",
+        desc: "Initial commit"
+    },
+    {
+        emoji: "✨",
+        github: "sparkles",
+        desc: "New feature"
+    },
+    {
+        emoji: "🐛",
+        github: "bug",
+        desc: "Bugfix"
+    },
+    {
+        emoji: "👌",
+        github: "ok_hand",
+        desc: "Code review changes"
+    },
+    {
+        emoji: "📚",
+        github: "books",
+        desc: "Documentation"
+    },
+    {
+        emoji: "💡",
+        github: "bulb",
+        desc: "Documenting source code"
+    },
+    {
+        emoji: "🐎",
+        github: "racehorse",
+        desc: "Performance"
+    },
+    {
+        emoji: "🗃️",
+        github: "card_file_box",
+        desc: "Database"
+    },
+    {
+        emoji: "💄",
+        github: "lipstick",
+        desc: "Cosmetic"
+    },
+    {
+        emoji: "🚨",
+        github: "rotating_light",
+        desc: "Tests"
+    },
+    {
+        emoji: "🧪",
+        github: "test_tube",
+        desc: "Tests"
+    },
+    {
+        emoji: "🚦",
+        github: "vertical_traffic_light",
+        desc: "Unit tests"
+    },
+    {
+        emoji: "🎢",
+        github: "roller_coaster",
+        desc: "E2E tests"
+    },
+    {
+        emoji: "✅",
+        github: "white_check_mark",
+        desc: "Adding a test"
+    },
+    {
+        emoji: "✔️",
+        github: "heavy_check_mark",
+        desc: "Fix tests"
+    },
+    {
+        emoji: "✔️",
+        github: "heavy_check_mark",
+        desc: "Make a test pass"
+    },
+    {
+        emoji: "⚡",
+        github: "zap",
+        desc: "General update"
+    },
+    {
+        emoji: "🎨",
+        github: "art",
+        desc: "Improve format/structure"
+    },
+    {
+        emoji: "🔨",
+        github: "hammer",
+        desc: "Refactor code"
+    },
+    {
+        emoji: "🔥",
+        github: "fire",
+        desc: "Removing code/files"
+    },
+    {
+        emoji: "〰️",
+        github: "wavy_dash",
+        desc: "Code abstraction"
+    },
+    {
+        emoji: "💚",
+        github: "green_heart",
+        desc: "Continuous Integration"
+    },
+    {
+        emoji: "🔒",
+        github: "lock",
+        desc: "Security"
+    },
+    {
+        emoji: "⬆️",
+        github: "arrow_up",
+        desc: "Upgrading dependencies"
+    },
+    {
+        emoji: "⬇️",
+        github: "arrow_down",
+        desc: "Downgrading dependencies"
+    },
+    {
+        emoji: "👕",
+        github: "shirt",
+        desc: "Lint"
+    },
+    {
+        emoji: "👽",
+        github: "alien",
+        desc: "Translation"
+    },
+    {
+        emoji: "📝",
+        github: "pencil",
+        desc: "Text"
+    },
+    {
+        emoji: "🚑",
+        github: "ambulance",
+        desc: "Critical hotfix"
+    },
+    {
+        emoji: "🚀",
+        github: "rocket",
+        desc: "Deploying stuff"
+    },
+    {
+        emoji: "🍎",
+        github: "apple",
+        desc: "Fixing on MacOS"
+    },
+    {
+        emoji: "🐧",
+        github: "penguin",
+        desc: "Fixing on Linux"
+    },
+    {
+        emoji: "🏁",
+        github: "checkered_flag",
+        desc: "Fixing on Windows"
+    },
+    {
+        emoji: "🚧",
+        github: "construction",
+        desc: "Work in progress"
+    },
+    {
+        emoji: "👷",
+        github: "construction_worker",
+        desc: "Adding CI build system"
+    },
+    {
+        emoji: "📈",
+        github: "chart_with_upwards_trend",
+        desc: "Analytics or tracking code"
+    },
+    {
+        emoji: "➖",
+        github: "heavy_minus_sign",
+        desc: "Removing a dependency"
+    },
+    {
+        emoji: "➕",
+        github: "heavy_plus_sign",
+        desc: "Adding a dependency"
+    },
+    {
+        emoji: "🐳",
+        github: "whale",
+        desc: "Docker"
+    },
+    {
+        emoji: "🔧",
+        github: "wrench",
+        desc: "Configuration files"
+    },
+    {
+        emoji: "📦",
+        github: "package",
+        desc: "Package.json in JS"
+    },
+    {
+        emoji: "🔀",
+        github: "twisted_rightwards_arrows",
+        desc: "Merging branches"
+    },
+    {
+        emoji: "💩",
+        github: "hankey",
+        desc: "Bad code / need improv."
+    },
+    {
+        emoji: "⏪",
+        github: "rewind",
+        desc: "Reverting changes"
+    },
+    {
+        emoji: "💥",
+        github: "boom",
+        desc: "Breaking changes"
+    },
+    {
+        emoji: "♿",
+        github: "wheelchair",
+        desc: "Accessibility"
+    },
+    {
+        emoji: "🚚",
+        github: "truck",
+        desc: "Move/rename repository"
+    },
+    {
+        emoji: "📇",
+        github: "card_index",
+        desc: "Metadata"
+    },
+    {
+        emoji: "➰",
+        github: "curly_loop",
+        desc: "Code reuse"
+    },
+    {
+        emoji: "📲",
+        github: "calling",
+        desc: "Responsive design"
+    },
+    {
+        emoji: "🖥️",
+        github: "desktop_computer",
+        desc: "UI"
+    },
+    {
+        emoji: "🚸",
+        github: "children_crossing",
+        desc: "UX"
+    },
+    {
+        emoji: "🔖",
+        github: "bookmark",
+        desc: "Version tag"
+    },
+    {
+        emoji: "🔖",
+        github: "bookmark",
+        desc: "Release"
+    },
+];
